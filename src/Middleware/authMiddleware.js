d8a8fa8170a40b479756c54336142d5af71359d6
wederson <wedersoncabral@gmail.com>
2024-03-16 14:21:04 -0300

    Create the auth Middleware

---
+++ src/Middleware/authMiddleware.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+
+const authMiddleware = {};
+
+
+authMiddleware.JWT = (req,res,next) =>{
+        // Retrieve token from the 'Authorization' header
+        const authHeader = req.headers.authorization;
+        if (authHeader) {
+            const token = authHeader.split(' ')[1]; // Bearer <token>
+    
+            jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+
+                if (err)
+                    return res.sendStatus(403); // Forbidden if error occurs
+    
+                req.user = user;
+                next();
+            });
+        } else {
+            res.status(401); // Unauthorized if no token is provided
+        }
+    
+}
+
+module.exports = authMiddleware;
