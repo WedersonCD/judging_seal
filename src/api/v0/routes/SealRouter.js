5ff01d4353b8f4c40781caf450bf4740e16e268f
wederson <wedersoncabral@gmail.com>
2024-03-23 13:51:21 -0300

    Add delete seal route

--- src/api/v0/routes/SealRouter.js
+++ src/api/v0/routes/SealRouter.js
@@ -4,6 +4,7 @@ const router = require('express').Router();
 router.post('/',Seal.createSeal);
 router.get('/',Seal.getAllSeals);
 router.get('/:seal_name',Seal.getSealByName);
+router.delete('/',Seal.deleteSealById);
 
 
 module.exports = router;
