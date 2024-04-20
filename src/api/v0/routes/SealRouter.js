e63a777b0202761d8a3a3f1898dc816f9e04269b
wederson <wedersoncabral@gmail.com>
2024-04-20 06:12:35 -0300

    Add get seasl byId Router

--- src/api/v0/routes/SealRouter.js
+++ src/api/v0/routes/SealRouter.js
@@ -1,11 +1,11 @@
 const Seal = require('../controllers/SealController');
 const router = require('express').Router();
 
-router.post('/',Seal.createSeal);
 router.get('/',Seal.getAllSeals);
+router.get('/:sealId',Seal.getSealById);
+
+router.post('/',Seal.createSeal);
 router.put('/',Seal.updateSeal);
-router.get('/:seal-name',Seal.getSealByName);
 router.delete('/',Seal.deleteSealById);
-router.get('/opean-ocean',Seal.opeanOcean);
 
 module.exports = router;
